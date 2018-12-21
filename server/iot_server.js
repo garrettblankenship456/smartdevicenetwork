109d7019e7cb52da13436e13180bd85274dba2ac
garrettblankenship456 <g.blankenship456@gmail.com>
2018-12-20 21:35:25 -0600

    Added basic usage of server

--- server/iot_server.js
+++ server/iot_server.js
@@ -1,1 +1,21 @@
 // This file runs the server and allows each device to communicate with eachother
+
+// Requirements
+const net = require("net");
+const fs = require("fs");
+
+// Read config
+var config = JSON.parse(fs.readFileSync("serverconfig.json"));
+
+// Main code
+console.log("Setting up server");
+
+// Setup basic server
+var server = net.createServer((socket) => {
+  socket.write("Working great!\n");
+  socket.pipe(socket);
+});
+
+// Make the server listen
+console.log("Server listening| Port: " + config.port + ", IP: " + config.ip);
+server.listen(config.port, config.ip);
