6409b59d53677f9303381d53804ef0f51c31a517
garrettblankenship456 <g.blankenship456@gmail.com>
2018-12-23 13:48:02 -0600

    Fixed error with led script

--- iot_device/device_custom/led.py
+++ iot_device/device_custom/led.py
@@ -8,22 +8,31 @@ import iot
 os.system("gpio -g mode 18 out")
 
 # Define functions
-def led_on():
+def led_on(args):
     print("LED on")
     os.system("gpio -g write 18 1")
 
-def led_off():
+def led_off(args):
     print("LED off")
     os.system("gpio -g write 18 0")
+
+def led(args):
+    print("LED control")
+
+    if args[0] == "1":
+        os.system("gpio -g write 18 1")
+    elif args[0] == "0":
+        os.system("gpio -g write 18 0")
 
 # Setup device
 device = iot.IOT("10.0.0.191", 5623) # Define the ID, first argument is IP, second argument is the port
 device.start() # Start the connection to the IOT server
-device.setID("iot_script_test") # Set the ID of the IOT device
+device.setID("led_device") # Set the ID of the IOT device
 
 # Define functions with the device
 device.defineCommand("led_on", led_on)
 device.defineCommand("led_off", led_off)
+device.defineCommand("led", led)
 
 # Keep looping the take command so it doesnt quit
 try: # Make sure to cleanly exit
