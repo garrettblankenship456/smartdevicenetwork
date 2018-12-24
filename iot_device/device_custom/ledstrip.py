183b0e09a3081dd6fd6b5841b7d16d941a3370ec
Garrett Blankenship <g.blankenship456@gmail.com>
2018-12-24 03:46:16 +0000

    Fixed led strip errors

--- iot_device/device_custom/ledstrip.py
+++ iot_device/device_custom/ledstrip.py
@@ -2,12 +2,12 @@
 import sys
 import os
 from neopixel import *
-import argparse
+from time import sleep
 sys.path.insert(0, "..")
 import iot
 
 # Setup variables for pins
-LED_COUNT = 16
+LED_COUNT = 58
 LED_PIN = 21
 LED_FREQ_HZ = 800000
 LED_DMA = 10
@@ -31,21 +31,23 @@ def ledOn(args):
     print("Turning on LEDS")
     color = Color(int(args[0]), int(args[1]), int(args[2]))
     for i in args[3:]:
-        strip.setPixelColor(i, color)
+        strip.setPixelColor(int(i), color)
+	strip.show()
 def clear(args):
     print("LEDs clearing")
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, Color(0, 0, 0))
+	strip.show()
 
 # Setup device
 device = iot.IOT("10.0.0.191", 5623) # Define the ID, first argument is IP, second argument is the port
 device.start() # Start the connection to the IOT server
-device.setID("lcd_device") # Set the ID of the IOT device
+device.setID("ledstrip") # Set the ID of the IOT device
 
 # Define functions with the device
 device.defineCommand("led", ledOn)
 device.defineCommand("color", colorWipe)
-device.defineCommand("shutdown", shutdown)
+device.defineCommand("clear", clear)
 
 # Keep looping the take command so it doesnt quit
 try: # Make sure to cleanly exit
