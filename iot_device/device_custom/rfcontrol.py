5c576adf40e56f058d297feb87fca24016618f1b
garrettblankenship456 <g.blankenship456@gmail.com>
2019-12-04 20:22:55 -0600

    Added code stroage

--- iot_device/device_custom/rfcontrol.py
+++ iot_device/device_custom/rfcontrol.py
@@ -2,6 +2,7 @@
 import sys
 import time
 from rpi_rf import RFDevice
+import json
 import threading
 sys.path.insert(0, "..")
 import iot
@@ -15,12 +16,35 @@ rfTrans.enable_tx()
 # Receiving variables
 recvTime = None
 lastCodeReceived = None
+lastPulseLength = None
+lastProtocol = None
+
+# Data functions
+def getCodes():
+    # Returns all the codes in the json
+    dataFile = open("codes.json", "r")
+    data = dataFile.read()
+    data = json.loads(data)
+
+    dataFile.close()
+
+    return data
+
+def writeCodes(data:dict):
+    # Writes the data to the json file
+    dataFile = open("codes.json", "w")
+    data = json.dumps(data)
+    
+    dataFile.write(data)
+    dataFile.close()
 
 # Create receive thread
 def receiveData():
     # Open globals
     global recvTime
     global lastCodeReceived
+    global lastPulseLength
+    global lastProtocol
 
     # Receive the code with given parameters
     print("Listening")
@@ -28,6 +52,8 @@ def receiveData():
         # Check if code changed
         if rfRecv.rx_code_timestamp != recvTime:
             recvTime = rfRecv.rx_code_timestamp
+            lastPulseLength = rfRecv.rx_pulselength
+            lastProtocol = rfRecv.rx_proto
 
             lastCodeReceived = rfRecv.rx_code
             print("Received code:", lastCodeReceived)
@@ -43,8 +69,38 @@ def transmitDecimal(args):
     print("Transmitting code!")
     rfTrans.tx_code(int(args[0]), int(args[1]), int(args[2]))
 
-def receiveDecimal(args):
-    pass
+def addCode(args):
+    # Define data
+    name = args[0]
+
+    # Wait until button pressed
+    print("Listening to keypress")
+    startTime = (time.perf_counter() * 1000000)
+    
+    while recvTime < startTime:
+        time.sleep(0.1)
+
+    # Adds a code to a database
+    codes = getCodes()
+    codes[name] = {
+        "code": lastCodeReceived,
+        "pulse": lastPulseLength,
+        "proto": lastProtocol
+    }
+    writeCodes(codes)
+
+def sendCode(args):
+    # Get code list
+    codes = getCodes()
+
+    codeData = codes[args[0]] # Get data from name
+
+    # Only continue if data exists
+    if codeData == None: return
+
+    # Send pulse
+    print("Transmitting code:", codeData)
+    transmitDecimal([codeData["code"], codeData["proto"], codeData["pulse"]])
 
 def sendLastCode(args):
     # Skip if no last code
@@ -68,7 +124,7 @@ print("====================================================\n")
 
 # Define commands
 rfIOT.defineCommand("transmit", transmitDecimal)
-rfIOT.defineCommand("receive", receiveDecimal)
+rfIOT.defineCommand("add_code", addCode)
 rfIOT.defineCommand("send_last", sendLastCode)
 
 # Accept commands
